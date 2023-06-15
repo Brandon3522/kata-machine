9c05cbc42d5514ab8bc29c1f5989499adf73516c
Brandon3522 <77870751+Brandon3522@users.noreply.github.com>
2023-06-15 17:33:20 -0500

    Linear search and binary search implementation

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
+// O(log n)
 export default function bs_list(haystack: number[], needle: number): boolean {
+	let first = 0;
+	let last = haystack.length - 1;
 
+	while (first <= last) {
+		let mid = Math.floor((first + last) / 2);
+		if (haystack[mid] === needle) {
+			return true;
+		}
+		else if (needle < haystack[mid]) {
+			last = mid - 1;
+		}
+		else if (needle > haystack[mid]) {
+			first = mid + 1;
+		}
+	}
+
+	return false;
 }
