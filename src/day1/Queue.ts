7dc847cf7df306d359606f62bcdb7c10073716ed
Brandon3522 <77870751+Brandon3522@users.noreply.github.com>
2023-06-15 17:34:19 -0500

    Bubble sort, two crystal balls, and queue implementation.

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,50 @@
+type Node<T> = {
+	value: T,
+	next?: Node<T>,
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+		private head: Node<T> | undefined;
+		private tail: Node<T> | undefined;
 
     constructor() {
-    }
+			this.head = this.tail = undefined;
+			this.length = 0;
+		}
 
     enqueue(item: T): void {
+			const node = {value: item} as Node<T>; // Create node
+			this.length ++;
 
-}
+			if(!this.tail) {
+				this.tail = this.head = node;
+				return;
+			}
+
+			this.tail.next = node;
+			this.tail = node;
+		}
     deque(): T | undefined {
+			if (!this.head) {
+				return undefined;
+			}
 
-}
+			this.length--;
+
+			const head = this.head; // Save head
+			this.head = this.head.next; // Update head
+
+			// Free memory 
+			head.next = undefined; // Delete head
+
+			if (this.length === 0) {
+				this.tail = undefined;
+			}
+
+			return head.value; // Return value
+		}
     peek(): T | undefined {
-
-}
+			return this.head?.value;
+		}
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,36 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+// Question:
+	// Given two crystal ball that will break if dropped from high enough
+	// distance, determine the exact spot in which it will break in the most
+	// optimized way
 
+// Array consisting of true and false -> 1 and 0
+// Once a crystal ball reaches false, it will break after that point
+// Since we have two crystal balls, we can use a binary search
+// However, it still results in O(n)
+// So we need to jump by a different unit, sqrt n
+// Jump until break, then walk back until our last good point
+
+// O(sqrt n)
+export default function two_crystal_balls(breaks: boolean[]): number {
+	const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+
+	// Use first crystal ball to find break point
+	let i = jumpAmount;
+	for (; i < breaks.length; i+= jumpAmount) {
+		if (breaks[i]) {
+			break;
+		}
+	}
+
+	// Jump back sqrt n
+	i -= jumpAmount;
+
+	// Walk forward by sqrt n
+	for (let j = 0; j < jumpAmount && i < breaks.length; j++, i++) {
+		if (breaks[i]) {
+			return i; // Return index
+		}
+	}
+
+	return -1;
 }
