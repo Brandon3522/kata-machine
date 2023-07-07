00441b8c128bfa81f0e03d870cb221a9314ca718
Brandon3522 <77870751+Brandon3522@users.noreply.github.com>
2023-07-07 12:14:57 -0500

    Quicksort complete

--- src/__tests__/QuickSort.ts
+++ src/__tests__/QuickSort.ts
@@ -1,6 +1,11 @@
 import quick_sort from "@code/QuickSort";
 
 test("quick-sort", function () {
+    const arr1 = [8, 6, 9, 4, 5];
+
+    quick_sort(arr1);
+    expect(arr1).toEqual([4, 5, 6, 8, 9]);
+
     const arr = [9, 3, 7, 4, 69, 420, 42];
 
     debugger;
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,47 @@
+
+function qs(arr: number[], lo: number, hi: number): void {
+	// Base case
+	if (lo >= hi) {
+		return;
+	}
+
+	const pivotIdx = partition(arr, lo, hi);
+
+	// Not including the pivot
+	qs(arr, lo, pivotIdx - 1);
+	qs(arr, pivotIdx + 1, hi);
+}
+
+// Returns the pivot index
+function partition(arr: number[], lo: number, hi: number): number {
+	const pivot = arr[hi];
+
+	let idx = lo - 1; // Gives low of split both arrays
+
+	// Walk from low to high without including the pivot
+	for (let i = lo; i < hi; i++) {
+		// Compare each element to the pivot
+		if (arr[i] <= pivot) {
+			idx++;
+			const tmp = arr[i];
+			arr[i] = arr[idx];
+			arr[idx] = tmp;
+		}
+	}
+
+	idx++;
+	arr[hi] = arr[idx];
+	arr[idx] = pivot;
+
+	return idx;
+}
+
+// Low and high are inclusive
 export default function quick_sort(arr: number[]): void {
+	qs(arr, 0, arr.length - 1);
+
+
+
+
 
 }
