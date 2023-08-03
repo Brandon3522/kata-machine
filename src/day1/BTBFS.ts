3bd347e49adfd60c39c8fa0dbff5427599ac5a0e
Brandon3522 <77870751+Brandon3522@users.noreply.github.com>
2023-08-03 18:48:39 -0500

    Breadth first search and binary tree comparison

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,27 @@
+// Get next item from queue
+// Do operation
+// Push left and right child into queue
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  // Queue
+  const q = [head];
 
+  while (q.length) {
+	// Remove first element from queue
+    const curr = q.shift() as BinaryNode<number>;
+
+    // Search
+    if (curr.value === needle) {
+      return true;
+    }
+
+	// Check for children
+    if (curr.left) {
+      q.push(curr.left);
+    }
+    if (curr.right) {
+      q.push(curr.right);
+    }
+  }
+
+  return false;
 }
--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,24 @@
+
+
+
+// Compare binary trees to check if they are equal in structure and values
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
 
+	//Base cases
+	// Structure check-> Cant recuse any further, structurally the same
+	if (a == null && b == null) {
+		return true;
+	}
+
+	// Structure check-> Structurally not the same
+	if (a == null || b == null) {
+		return false;
+	}
+
+	// Value check
+	if (a.value != b.value) {
+		return false;
+	}
+
+	return compare(a.left, b.left) && compare(a.right, b.right); 
 }
