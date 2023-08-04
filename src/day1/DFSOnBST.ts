b8429085365ccdeb27e4a1b9459ac30f9de9493b
Brandon3522 <77870751+Brandon3522@users.noreply.github.com>
2023-08-03 20:17:22 -0500

    Depth first search on binary search tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,21 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+	
+	if (!curr) { // null, made it to the bottom of the tree
+		return false;
+	}
 
+	if (curr.value === needle) { // Found value
+		return true;
+	}
+	
+	// Traversal
+	if (curr.value < needle) { // Search right
+		return search(curr.right, needle);
+	}
+	
+	return search(curr.left, needle); // Search left
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+	return search(head, needle);
 }
